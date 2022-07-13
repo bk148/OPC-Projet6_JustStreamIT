e157b6acfe45baf6bc4d0811ef7d59c314f0e8d6
bkatende <98980843+bkatende@users.noreply.github.com>
2022-07-13 11:07:14 +0200

    JustStreamIT

---
+++ app.js
@@ -0,0 +1,129 @@
+
+// base url du site
+const base_url = "http://localhost:8000/api/v1/titles/" 
+const base_url_banner = "http://localhost:8000/api/v1/titles/?&page_size=200"
+const page_size = "&page_size=22"
+
+
+// requests for movies data
+const requests = {
+  fetchBestmovies: base_url + "?sort_by=-imdb_score" + page_size,
+  fetchHorreur_Byvotes: base_url + "?genre=horror&sort_by=-votes" + page_size,
+  fetchAction_Byvotes: base_url + "?genre=action&sort_by=-votes"+ page_size,
+  fetchAdventure_Byvotes: base_url + "?genre=adventure&sort_by=-votes" + page_size,
+};
+
+function Modal(){
+  let modal = document.getElementById('film-modal');
+  let infoBtn = document.getElementsByClassName('info row__posterLarge');
+  
+  console.log('images btn')
+  console.log(infoBtn);
+
+  for (let i = 0; i < infoBtn.length; i++) {
+    if (!infoBtn[i]) {
+        break;
+    }
+    infoBtn[i].addEventListener('click', async function() {
+      fetch(base_url + this.id).then(async function (response) {
+
+          let res = await response.json()
+          console.log('response');
+          console.log(res);
+
+          const content = `
+                <div class="modal__content">
+                    <span class="modal__close">&times;</span>
+                    <div class="modal__data">
+                        <img class="modal__data--img" src="${res.image_url}" alt="con.title">
+                        <div class="modal__data--details">
+                            <p>Titre : ${res.title}</p>
+                            <p>Genre(s) : ${res.genres}</p>
+                            <p>Date de publication : ${res.date_published}</p>
+                            <p>Score : ${res.rated}</p>
+                            <p>Score Imbd : ${res.imdb_score}</p>
+                            <p>Directeur(s) : ${res.directors}</p>
+                            <p>Acteurs : ${res.actors}</p>
+                            <p>Durée : ${res.duration} minutes</p>
+                            <p>Pays d orgine : ${res.countries}</p>
+                            <p>Box office: ${(res.worldwide_gross_income) ? res.worldwide_gross_income : "Pas disponible"}</p>
+                            <p>Description : ${res.description}</p>
+                        <div>    
+                    </div>
+                </div>`
+          modal.innerHTML = content
+          modal.style.display = "block";
+          document.getElementsByClassName("modal__close")[0].addEventListener('click', function () {
+              modal.style.display = "none";
+          });
+
+          console.log('inserted modal');
+          console.log(modal);
+
+      });
+    });
+  }
+}
+
+//banner_image_movies
+const api_data = fetch(base_url);
+api_data.then((Response) => {
+  const dataRequest = Response.json();
+  dataRequest.then((reponseApi) => {
+    const setMovie = reponseApi.results[Math.floor(Math.random() * reponseApi.results.length - 1)];
+    var banner = document.getElementById("banner_image");
+    banner.style.backgroundImage = "url("+ setMovie.image_url + ")";
+    banner_title.innerHTML = setMovie.title;
+
+  })
+})
+
+function fetchCategory(categoryUrl, categoryTitle) {
+  // 1 ere categorie choisie
+  return fetch(categoryUrl)
+      .then((Response) => Response.json())
+      .then((data) => {
+        const headrow = document.getElementById("headrow");
+        const row = document.createElement("div");
+        row.className = "row";
+        row.classList.add("Films");
+        headrow.appendChild(row);
+        const title = document.createElement("h2");
+        title.className = "row__title";
+        title.innerText = categoryTitle;
+        row.appendChild(title);
+        const row_posters = document.createElement("div");
+        row_posters.className = "row__posters";
+        row.appendChild(row_posters);
+        data.results.forEach(movie => {
+          const poster = document.createElement("img");
+          poster.className = "info row__posterLarge";
+          var s2 = movie.id;
+          poster.id = s2;
+          poster.src = movie.image_url
+          row_posters.appendChild(poster);
+        });
+      });
+}
+
+fetchCategory(requests.fetchBestmovies, 'Films les mieux notés')
+.then(function(){
+  Modal()
+}).then(async function() {
+  fetchCategory(requests.fetchHorreur_Byvotes, 'Horreur')
+  .then(function(){
+    Modal();
+  }).then(fetchCategory(requests.fetchAdventure_Byvotes, 'Adventure')
+  ).then(async function(){
+    fetchCategory(requests.fetchAction_Byvotes, 'Action')
+    .then(function(){
+      Modal()
+    })
+  })
+})
+
+
+
+
+
+
